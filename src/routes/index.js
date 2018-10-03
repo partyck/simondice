0800813ecd1325d9b90fa3890dea4ac7fa88b049
rndae <raghievox@gmail.com>
2018-10-03 15:16:49 -0400

    Subir package

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Tinder Umss'}); 
+    res.render('index', { title: 'Simon dice'}); 
+});
+
+router.post('/', (req, res) => {       
+    //res.render('index', "No se encontro a nadie");
 });
 
 
