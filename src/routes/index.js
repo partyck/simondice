74e21e4bc9d8bd1d85c4b834c3bbce8de1ce8119
jcgpm <sistema3015@gmail.com>
2018-10-04 19:30:12 -0400

    cambios

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Tinder Umss'}); 
+});
+
+router.post('./',(req res)=>{
+
+});
+
+module.exports = router;    
