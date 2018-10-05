e5540479493dbe5e77bfcd441947f0ccfa8d1810
jcgpm <sistema3015@gmail.com>
2018-10-05 08:42:56 -0400

    emparejar v1

---
+++ src/model/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = Schema({
+  nombre: String,
+  carrera: String,
+  semestre: Number
+});
+
+module.exports = mongoose.model('users', UserSchema);
