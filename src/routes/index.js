6bc2332a5b1814183f0eb9f0b85f04953642ab00
rndae <raghievox@gmail.com>
2018-10-12 03:57:29 -0400

    interfaces actualizadas, estructura de modulos mejorada

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,9 @@
 const express = require('express');
 
-var User = require('../model/user');
-
 const router = express.Router();
 
+var rutaEmpareja = require('./user/empareja');
 
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Simon dice'}); 
-});
-
-router.post('/', (req, res) => {     
-    User.find({}, function(err, users) {
-        if (err) throw err;
-        
-        console.log(users);
-        
-        if (users.length > 0) {
-            res.send("Se encontro a: " + users[0].getNombre());
-            return;
-        } else {
-            res.send("Sigue participando");
-            return;
-        }
-    });
-});
+router.use('/', rutaEmpareja);
 
 module.exports = router;    
