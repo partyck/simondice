31b8bb9ceb2fe1c4bd7656c99cb185465634d430
rndae <raghievox@gmail.com>
2018-10-25 01:38:26 -0400

    archivos estaticos disponibles desde la nueva carpeta, 'public'

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 
-<<<<<<< HEAD
 const router = express.Router();
-=======
+
 router.get('/', (req, res) => {
     res.render('index', { title: 'Simon Dice'}); 
 });
->>>>>>> 70a0b9483bd07aef928f8cc777c4ee524f763813
 
 var rutaEmpareja = require('./user/empareja');
 
