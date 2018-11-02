6b1b1cf35c9635ab1443cf991a59b4b949a04bbb
patryck808 <patrick.cuellaro@gmail.com>
2018-11-02 13:19:18 -0400

    mejora en el route de citas

--- src/routes/user/citas.js
+++ src/routes/user/citas.js
@@ -2,8 +2,13 @@ const express = require('express');
 const Date = require('../../models/date');
 const router = express.Router();
 
-router.get('/citas', (req, res) => {
-    res.render('user/citas'); 
+router.get('/citas', async (req, res) => {
+    var dates = await Date.find();
+    res.render('user/citas', {
+        dates
+    }); 
 });
+
+
 
 module.exports = router;
