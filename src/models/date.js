e341f12b1693bddbdcc7c448acd635ba9a6973ec
patryck808 <patrick.cuellaro@gmail.com>
2018-11-02 19:26:38 -0400

    integracion vista de citas con base de datos

--- src/models/date.js
+++ src/models/date.js
@@ -2,13 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DateSchema = Schema({
-  /**
-  * Falta implementar
-  *
-  */
-}, 
-  { collection : 'dates' }
-);
+  idSolicitante: Number,
+  idSolicitado: Number,
+  place: String,
+  date: String,
+  time: String,
+  state1: {
+    type: "String",
+    default: "null" 
+  },
+  state2: {
+    type: "String",
+    default: "null" 
+  }, 
+});
 
-var Date = mongoose.model("Date", DateSchema);
+var Date = mongoose.model("dates", DateSchema);
 module.exports = Date;
--- src/routes/user/citas.js
+++ src/routes/user/citas.js
@@ -4,9 +4,24 @@ const router = express.Router();
 
 router.get('/citas', async (req, res) => {
     var dates = await Date.find();
+    console.log(dates);
     res.render('user/citas', {
         dates
     }); 
+});
+
+router.get('/accept/:id', async (req, res, next) => {
+    let { id } = req.params;
+    const date = await Date.findById(id);
+    console.log (date);
+    if (date.state1 == "accept"){
+        date.state2 = "accept";
+    }else{
+        date.state1 = "accept";
+    }
+    date.state1 = "accept";
+    await date.save();
+    res.redirect('/citas');
 });
 
 
