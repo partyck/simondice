67d40465e2cc2199e4b176869a84dcffc70a44ba
aliciaLO7 <ali.lim.ore@gmail.com>
2018-11-02 04:50:22 -0400

    Fix Interfaz

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const User = require ('../models/user');
+
 router.get('/', (req, res) => {
     res.render('home', { title: 'Simon Dice'}); 
 });
@@ -10,6 +12,11 @@ router.get('/registrar', (req, res) => {
     res.render('./registrar'); 
 });
 
+router.post('/guardar',async(req,res) => {
+    const user = new User(req.body);
+    await user.save();
+    res.send('guardado');
+})
 var rutaEmpareja = require('./user/empareja');
 
 router.use('/', rutaEmpareja);
