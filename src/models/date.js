250bc50b80a7b5d733c4dda388e07120f56968d9
juan <sistema3015@gmail.com>
2018-11-02 19:27:50 -0400

    actualizacion de generar cita

--- src/models/date.js
+++ src/models/date.js
@@ -4,9 +4,17 @@ const Schema = mongoose.Schema;
 const DateSchema = Schema({
   idsolicitante: Number,
   idsolicitado: Number,
-  lugar: String,
-  hora: Date,
-  estado: String
+  place: String,
+  date: String,
+  time: String,
+  estado1: {
+    type: String,
+    default: "null"
+  },
+  estado2: {
+    type: String,
+    default: "null"
+  }
 } );
 
 var Date = mongoose.model("Date", DateSchema);
--- src/routes/user/generarcita.js
+++ src/routes/user/generarcita.js
@@ -1,38 +1,85 @@
 const express = require('express');
 
 var place = require('../../models/places');
+const Date = require('../../models/date');
+//var hora = require('./hora');
 
 const router = express.Router();
 //const mongoose = require('mongoose');
 var lugares;
+var fecha;
+var hora;
 
 router.get('/emparejar', (req, res) => {
-    res.render('user/empareja', { title: 'Simon dice: Emparejate'}); 
+    var places = [{
+        "lugar" : "bloque antiguo fcyt"
+    },{
+        "lugar" : "paseo autonomico"
+    },{
+        "lugar" : "edificio nuevo fcyt"
+    },{
+        "lugar" : "parqueo fcyt" 
+    },{
+        "lugar" : "puerta principal fcyt"
+    }];
+    places.forEach(async function(lugar){
+        var l = new place(lugar);
+        await l.save()
+    });
+    generarCita("6516","1616"); 
 });
 
-router.post('/emparejar', (req, res) => {     
-    place.find({}, function(err, places) {
+ var buscarLugar = function(){     
+     place.find({}, function(err, places) {
         if (err) {
             console.log("No se puedo realizar la operacion find()");
             throw err;
         }
-        console.log(places);
        if (places.length > 0) {
-        place.count().exec(function(err, resultCount) {
+         place.count().exec(function(err, resultCount) {
             var rand = Math.floor(Math.random() *resultCount);
-            place.findOne().skip(rand).exec(function(err, result) {
-              console.log(result);
-              lugares = result;
-              res.send("felicidades usted encontro el amor fue emparejado con : " + lugares);
+             place.findOne().skip(rand).exec(function(err, result) {
+              console.log(result.lugar);
+              lugares = result.lugar;
               return;
             });
           });
     } else {
-        res.send("Sigue participando lo siento no pudimos encontrar su pareja ideal");
+        res.send("no se encontro lugar");
         return;
     }
 
     });
-});
+};
+
+var generarCita = async function (idSolicitante, idSolicitado){
+    buscarLugar();
+    buscarFecha();
+    buscarHora();
+    const q = new Date({
+        "idsolicitante": idSolicitante,
+        "idsolicitado": idSolicitado,
+        "place": lugares,
+        "date": fecha,
+        "time": hora
+     });
+    await q.save();
+};
+
+   var buscarFecha = function(){
+    fecha = "25/12/2018";
+    var hoy = new Date();
+    console.log(hoy);
+
+};
+
+   var buscarHora =function (){
+    hora = "14:00";
+
+    var ho = new Date();
+
+
+};
+
 
 module.exports = router;
