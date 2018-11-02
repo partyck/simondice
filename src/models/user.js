0f9ca335f912dbf2d1d8fa3ec6eb7a514a78a837
rndae <raghievox@gmail.com>
2018-11-01 21:24:31 -0400

    model user actualizado; codigo refactorizado

--- src/models/user.js
+++ src/models/user.js
@@ -4,21 +4,25 @@ const Schema = mongoose.Schema;
 const UserSchema = Schema({
   id: Number,
   nombre: String,
+  sexo: String,
+  fechanacimiento: Date,
   carrera: String,
   semestre: {
     type: Number,
     default: 1
   }
 }, 
-  { collection : 'usuarios' }
-  /**
-  * Es necesario modificar el nombre de la
-  * coleccion a users, o lo que decidan.
-  */
+  { collection : 'users' }
 );
 
 UserSchema.methods.getNombre = function() {
-return this.nombre;
+    return this.nombre;
+};
+
+UserSchema.methods.getEdad = function() {
+    var edad = (new Date()).getTime() - fechanacimiento.getTime();
+    edad = (((edad / 60000) / 60 ) / (24*365));
+    return edad;
 };
 
 var User = mongoose.model("User", UserSchema);
