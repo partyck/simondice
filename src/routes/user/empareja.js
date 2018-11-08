bb02e34dddf54235a60047c706b49cb42ea62757
rndae <raghievox@gmail.com>
2018-11-08 14:46:52 -0400

    Interfaz corregida

--- src/routes/user/empareja.js
+++ src/routes/user/empareja.js
@@ -5,7 +5,7 @@ const registroUsuarios = require('./registro');
 const router = express.Router();
 
 router.get('/emparejar', (req, res) => {
-    res.render('user/empareja', { title: 'Simon dice: Emparejate'}); 
+    res.render('user/empareja', {usrEncontrado: -1}); 
 });
 
 router.post('/emparejar', (req, res) => {
@@ -13,10 +13,10 @@ router.post('/emparejar', (req, res) => {
     console.log("Se emparejo al usuario 4 con " + idUsuarioSolicitado);
         console.log("POST en /emparejar");
         if (idUsuarioSolicitado != "") {
-            res.render('user/empareja', {usuarioEncontrado: true});
+            res.render('user/empareja', {usrEncontrado: 1});
             return;
         } else {
-            res.render('user/empareja', {usuarioEncontrado: false});
+            res.render('user/empareja', {usrEncontrado: 0});  
             return;
         }
     });
