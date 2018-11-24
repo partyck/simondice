7dbc655e3f8a92e2cd1ec1046c4fd5070c36c36a
patryck808 <patrick.cuellaro@gmail.com>
2018-11-24 02:26:57 -0400

    funcion aceptar implementado

--- src/routes/user/citas.js
+++ src/routes/user/citas.js
@@ -1,42 +1,42 @@
 const express = require('express');
 const Appointment = require('../../models/appointment');
 const router = express.Router();
+let userId = '2';
 
 router.get('/citas', async (req, res) => {
-    // let userId = req.user._id;
-    await Appointment.find({
-        $or: [
-            { idApplicant: '2' },
-            { idRequested: '2' }]
-    },
-        function (err, dates) {
-            console.log(dates);
-            res.render('user/citas', {
-                dates
-            });
-        });
-});
-
-router.get('/accept/:id', async (req, res) => {
-    // let userId = req.user._id;
-    let userId = '2';
-    let { id } = req.params;
-    await Appointment.findById(id).then(async date => {
-        if (userId == date.idApplicant){
-            date.status1 = "accept";
-        }
-        if(userId == date.idRequested){
-            date.status2 = "accept";
-        }
-        await date.save();
-        res.redirect('/citas');
+  // let userId = req.user._id;
+  await Appointment.find({
+    $or: [
+      { idApplicant: userId },
+      { idRequested: userId }]
+  },
+    function (err, dates) {
+      console.log(dates);
+      res.render('user/citas', {
+        dates
+      });
     });
 });
 
-router.get('/refuse/:id', async (req, res) => {
-    let { id } = req.params;
-    await Appointment.remove({ _id: id });
+router.get('/accept/:id', async (req, res) => {
+  // let userId = req.user._id;
+  let { id } = req.params;
+  await Appointment.findById(id).then(async date => {
+    if (userId == date.idApplicant) {
+      date.status1 = "accept";
+    }
+    if (userId == date.idRequested) {
+      date.status2 = "accept";
+    }
+    await date.save();
     res.redirect('/citas');
+  });
+});
+
+router.get('/refuse/:id', async (req, res) => {
+  let { id } = req.params;
+  await Appointment.remove({ _id: id });
+  res.redirect('/citas');
 });
 
 module.exports = router;
