0754b1ed138580c2585e5e9b76d93951ac077730
aliciaLO7 <ali.lim.ore@gmail.com>
2018-11-25 07:41:14 -0400

    Estandarizacion hecha

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,53 +1,53 @@
 const User = require('../models/user');
 
-exports.index = function(req, res){
-    User.find({}, function(error, users){
-        if(error){
-           res.send('Ha surgido un error.');
-        }else{
-           res.render('user/empareja', {
-              users: users
-           });
-        }
-     })
+exports.index = function (req, res) {
+  User.find({}, function (error, users) {
+    if (error) {
+      res.send('Ha surgido un error.');
+    } else {
+      res.render('user/empareja', {
+        users: users
+      });
+    }
+  })
 };
 exports.create = function (req, res) {
   res.render('registrar');
 };
-exports.store = function(req, res){
-    var tipo = req.body.tipo_date;
-    var año;
-    if(tipo == "Anual"){
-        año = (req.body.semester) * 2;
-    }else{
-        año = req.body.semester;
+exports.store = function (req, res) {
+  var tipo = req.body.tipo_date;
+  var año;
+  if (tipo == "Anual") {
+    año = (req.body.semester) * 2;
+  } else {
+    año = req.body.semester;
+  }
+  const user = new User({
+    name: req.body.name,
+    birthdate: req.body.birthdate,
+    sex: req.body.sex,
+    course: req.body.course,
+    semester: año,
+    sexOrientation: req.body.sexOrientation,
+    minAge: req.body.minAge,
+    maxAge: req.body.maxAge,
+    email: req.body.email,
+    password: req.body.password
+  });
+  user.save(function (err) {
+    if (err) {
+      console.log(err);
+      res.render('registrar', { estado: 0 });
+    } else {
+      res.render('home', { estado: 1 });
     }
-    const user = new User({
-        name: req.body.name,
-        birthdate: req.body.birthdate,
-        sex: req.body.sex,
-        course: req.body.course,
-        semester: año,
-        sexOrientation: req.body.sexOrientation,
-        minAge: req.body.minAge,
-        maxAge: req.body.maxAge,
-        email: req.body.email,
-        password: req.body.password
-    });
-    user.save(function(err){
-        if(err){
-            console.log(err);
-            res.render('registrar',{estado:0});
-        }else{ 
-            res.render('home',{estado:1});
-        }
-     });
+  });
 };
-exports.login = function(req, res){
-    
- };
-exports.show = function(req, res){
-   //
+exports.login = function (req, res) {
+
+};
+exports.show = function (req, res) {
+  //
 };
 exports.edit = function (req, res) {
   //
