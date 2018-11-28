69fcb9f7338b9c04246e1024805c4c9e73bda112
patryck808 <patrick.cuellaro@gmail.com>
2018-11-28 08:02:22 -0400

    registro implementado con passport y login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,4 @@
 const router = require('express').Router();
-// const rutaEmpareja = require('./user/empareja');
-// const rutaCitas = require('./user/citas');
-// const User = require('../models/user');
 const UserController = require('../controllers/user');
 const passport = require('passport');
 
@@ -11,25 +8,23 @@ router.get('/', (req, res) => {
 
 router.get('/user', UserController.index);
 router.get('/user/create', UserController.create);
+router.get('/login', UserController.loginGet);
+
 router.post('/user', passport.authenticate('local-registro', {
   successRedirect: '/emparejar',
   failureRedirect: '/user/create',
   passReqToCallback: true
 }));
-router.get('/logout', (req, res, next) => {
-  req.logout();
-  res.redirect('/');
-});
-router.get('/user/:id', UserController.show);
-router.get('/user/:id/edit', UserController.edit);
-router.put('/user/:id', UserController.update);
-router.delete('/user/:id', UserController.destroy);
+
 router.post('/login', passport.authenticate('local-login', {
   successRedirect: '/emparejar',
   failureRedirect: '/login',
   passReqToCallback: true
 }));
 
-router.get('/login', UserController.loginGet);
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;  
