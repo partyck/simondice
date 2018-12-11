b0b321f4b21a4c449f0c9b201ee7a7743b9b0824
patryck808 <patrick.cuellaro@gmail.com>
2018-12-11 11:07:08 -0400

    nuevos schemas e interfaz de registro modificada

---
+++ src/models/faculty.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const facultySchema = Schema({
+  name: String
+
+},
+  { collection: 'facultys' }
+);
+
+var faculty = mongoose.model("Facultys", facultySchema);
+module.exports = faculty;
