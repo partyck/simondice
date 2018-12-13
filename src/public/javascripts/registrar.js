582849929c6127074f5ee0fc04c50f5a48fa160e
patryck808 <patrick.cuellaro@gmail.com>
2018-12-13 11:27:29 -0400

    recuperacion del evento de seleccion de facultad

---
+++ src/public/javascripts/registrar.js
@@ -0,0 +1,15 @@
+function facultySelected(){
+  var idFaculty = document.getElementById("faculty").value;
+  console.log(idFaculty);
+  var career = document.getElementById("career");
+  career.disabled=false;
+
+  var careers = ;
+  console.log(careers);
+  
+}
+
+// $.get("/getvar", function(data){
+//   var careers = data.careers;
+//   console.log(careers);
+// });
