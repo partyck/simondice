67ce6a0d35fee7d9c875caf679ab7e51346a75d4
Usuario <Usuario>
2018-12-14 08:50:58 -0400

    verificando las preferencias

--- src/routes/user/mapapreferencias.js
+++ src/routes/user/mapapreferencias.js
@@ -9,11 +9,22 @@ class MapaPreferencias {
   insertarRegla(tipoPreferencia, preferenciaA, preferenciaB, valor) {
     /**
     * - Hay que controlar si existe la clave preferenciaA-preferenciaB, o
+    
     * preferenciaB-preferenciaA, si es asi, sobreescribir en ese lugar. Si no,
     * agregar el valor con la nueva clave.
     * - Para agregar, hariamos, por ejemplo:
     * this._mapa[preferencia][preferenciaA + preferenciaB] = valor;
     */
+   if(preferenciaA.getid() === preferenciaB.getid()
+      || preferenciaB.getid() === preferenciaA.getid() )
+    {
+      
+
+    }else{
+      this._mapa[preferrencia][preferenciaA+preferenciaB]=valor
+    }
+
+    }
   }
   
   obtenerValorRegla(tipoPreferencia, preferenciaA, preferenciaB) {
