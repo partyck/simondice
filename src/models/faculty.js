6dd36efbff834048d0ec551bab5519eeba589ef4
patryck808 <patrick.cuellaro@gmail.com>
2018-12-17 10:31:20 -0400

    control de las carreras en el lado del cliente

--- src/models/faculty.js
+++ src/models/faculty.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const facultySchema = Schema({
   name: String
-
 },
   { collection: 'facultys' }
 );
--- src/public/javascripts/registrar.js
+++ src/public/javascripts/registrar.js
@@ -1,15 +1,10 @@
 function facultySelected() {
   var idFaculty = document.getElementById("faculty").value;
-  console.log(idFaculty);
   var careerCombobox = document.getElementById("career");
   careerCombobox.disabled = false;
   careerCombobox.innerHTML = '<option value="">Escoge una Carrera</option>';
-  // var careers = document.getElementById('carreras1').value;
-  // console.log(careers);
   var localObj = JSON.parse($("#myLocalDataObj").val());
   localObj.forEach(career => {
-    console.log(career.idFaculty);
-    console.log(idFaculty);
     if(career.idFaculty === idFaculty){
       var option = document.createElement('option');
       option.value = career._id;
@@ -18,8 +13,3 @@ function facultySelected() {
     }
   });
 }
-
-// $.get("/getvar", function(data){
-//   var careers = data.careers;
-//   console.log(careers);
-// });
