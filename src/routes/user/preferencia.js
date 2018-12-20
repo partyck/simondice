9067023fc3cc22b12844985bc67240225f9c52d0
aliciaLO7 <ali.lim.ore@gmail.com>
2018-12-20 09:29:09 -0400

    HU terminada con funcionalidad

--- src/routes/user/preferencia.js
+++ src/routes/user/preferencia.js
@@ -6,6 +6,7 @@ const PreferenceUser = require('../../models/preferenceUser');
 const router = express.Router();
 
 router.get('/preferencias', UserController.isAuthenticated, async (req, res) => {
+  let userId = req.user.id;
   preferencesCategories = await PreferenceCategory.find();
   preferences = await Preference.find();
   preferencesUsers = await PreferenceUser.find();
@@ -16,38 +17,32 @@ router.get('/preferencias', UserController.isAuthenticated, async (req, res) => 
 });
 
 router.post('/preferencias', UserController.isAuthenticated, async (req, res) => {
-  preferencesCategories = await PreferenceCategory.find();
-  preferences = await Preference.find();
-  preferencesUsers = await PreferenceUser.find();
   let userId = req.user.id;
   let preferenceUser = new PreferenceUser({
     idUser: userId,
     idPreference: req.body.preference
   });
   preferenceUser.save(function(err){
-    console.log(err);
-    res.render('user/preferencia',{
-      preferencesCategories:preferencesCategories,
-      preferences:preferences,
-      preferencesUsers:preferencesUsers});
+    res.redirect('/preferencias');
   });
 });
 
 router.post('/preferencia', UserController.isAuthenticated, async (req, res) => {
-  preferencesCategories = await PreferenceCategory.find();
-  preferences = await Preference.find();
-  preferencesUsers = await PreferenceUser.find();
   let preference = new Preference({
     name: req.body.preferenceN,
     idCategory: req.body.preferenceCat
   });
   preference.save(function(err){
     console.log(err);
-    res.render('user/preferencia',{
-      preferencesCategories:preferencesCategories,
-      preferences:preferences,
-      preferencesUsers:preferencesUsers});
+    res.redirect('preferencias');
   });
+});
+
+router.get('/preferencias/:id', UserController.isAuthenticated, async (req, res) => {
+  //let userId = req.user.id;
+  let { id } = req.params;
+  await PreferenceUser.deleteOne({_id: id}, 
+    res.redirect('/preferencias'));
 });
 
 module.exports = router;
